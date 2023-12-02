9b88d08b20205aea76f2aac739720e0001e455cf
Lion <31923080+imgradually@users.noreply.github.com>
2023-12-02 15:33:34 -0800

    Update load.py

--- src/helpers/load.py
+++ src/helpers/load.py
@@ -2,6 +2,19 @@ import argparse
 import gzip
 import pickle
 from tqdm import tqdm
+from fractions import Fraction
+
+def str2float(str):
+    return float(Fraction(str))
+
+def cleanData(d):
+    d['beer/ABV'] = float(d['beer/ABV'])
+    d['review/appearance'] = str2float(d['review/appearance'])
+    d['review/aroma'] = str2float(d['review/aroma'])
+    d['review/palate'] = str2float(d['review/palate'])
+    d['review/taste'] = str2float(d['review/taste'])
+    d['review/overall'] = str2float(d['review/overall'])
+    return d
 
 def loadAndSave(count, path):
     """
@@ -9,9 +22,10 @@ def loadAndSave(count, path):
     """
     data = []
     with gzip.open(path, mode='rt') as f:
-        lines = f.readlines()
-        for i in tqdm(range(count)):
-            data.append(eval(lines[i])) 
+        for line in tqdm(f):
+            if(len(data) == count): break
+            d = eval(line)
+            if('beer/ABV' in d and d['beer/ABV'] != '-'): data.append(cleanData(d)) 
     with open("data/data.pkl", mode='wb') as f:
         pickle.dump(data, f)
 
